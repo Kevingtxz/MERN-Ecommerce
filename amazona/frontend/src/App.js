91e82af777ffe4034f9934adc6d5abaacd63d82e
Kevin <kevingtxz@gmail.com>
2021-02-08 12:01:58 -0300

    Adding Routes

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -1,31 +1,28 @@
 import React from 'react';
-import data from './data';
-import Product from './components/Product';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div className="grid-container">
-        <header className="row">
-            <div>
-                <a className="brand" href="/">amazona</a>
-            </div>
-            <div>
-                <a href="/cart">Cart</a>
-                <a href="/signin">Sing In</a>
-            </div>
-        </header>
-        <main>
-            <div className="row center">
-              {
-                data.products.map(product => (
-                    <Product key={product._id} product={product}/>
-                ))
-              }
-              </div>
-        </main>
-        <footer className="row center">All right reserved</footer>
-    </div>
-     
+    <BrowserRouter>
+        <div className="grid-container">
+            <header className="row">
+                <div>
+                    <a className="brand" href="/">amazona</a>
+                </div>
+                <div>
+                    <a href="/cart">Cart</a>
+                    <a href="/signin">Sing In</a>
+                </div>
+            </header>
+            <main>
+                <Route path="/product/:id" component={ProductScreen}/>
+                <Route path="/" component={HomeScreen} exact/>
+            </main>
+            <footer className="row center">All right reserved</footer>
+        </div>
+    </BrowserRouter>
   );
 }
 
