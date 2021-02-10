1eeea354c892b19221ba01e0b5e740fb6a84d46a
Kevin <kevingtxz@gmail.com>
2021-02-09 23:55:00 -0300

    React Redux in Cart

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -1,31 +1,38 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import CartScreen from './screens/CartScreen';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import CartScreen from './screens/CartScreen'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
-function App() {
+function App () {
+  const cart = useSelector(state => state.cart)
+  const { cartItems } = cart
   return (
     <BrowserRouter>
-        <div className="grid-container">
-            <header className="row">
-                <div>
-                    <a className="brand" href="/">amazona</a>
-                </div>
-                <div>
-                    <a href="/cart">Cart</a>
-                    <a href="/signin">Sing In</a>
-                </div>
-            </header>
-            <main>
-                <Route path="/cart/:id?" component={CartScreen}/>
-                <Route path="/product/:id" component={ProductScreen}/>
-                <Route path="/" component={HomeScreen} exact/>
-            </main>
-            <footer className="row center">All right reserved</footer>
-        </div>
+      <div className='grid-container'>
+        <header className='row'>
+          <div>
+            <Link className='brand' href='/'>amazona</Link>
+          </div>
+          <div>
+            <Link href='/cart'>Cart
+              {cartItems.length > 0 && (
+                <span className='badge'>{cartItems.length}</span>
+              )}
+            </Link>
+            <Link href='/signin'>Sing In</Link>
+          </div>
+        </header>
+        <main>
+          <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/' component={HomeScreen} exact />
+        </main>
+        <footer className='row center'>All right reserved</footer>
+      </div>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
