554b2db75ca573059f9f471f70a110e482c42312
Kevin <kevingtxz@gmail.com>
2021-03-01 16:49:00 -0300

    MongoDB and basic User

--- amazona/backend/server.js
+++ amazona/backend/server.js
@@ -1,25 +1,39 @@
 import express from 'express'
 import data from './data.js'
+import mongoose from 'mongoose'
+import userRouter from './router/userRouter.js'
 
 const app = express()
+mongoose.connect('mongodb://localhost/amazona', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
 
 app.get('/api/products/:id', (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id)
-    if (product) {
+  if (product) {
     res.send(product)
-    } else {
+  } else {
     res.status(404).send({ message: 'Product Not Found' })
-    }
+  }
 })
 
 app.get('/api/products', (req, res) => {
   res.send(data.products)
 })
 
+app.use('/api/users', userRouter)
 app.get('/', (req, res) => {
   res.send('Server is ready')
 })
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message })
+})
+
 const port = process.env.PORT || 5000
+
 app.listen(port, () => {
   console.log(`Server at http://localhost:${port}`)
 })
